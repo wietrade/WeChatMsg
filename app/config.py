17e299eccfffac14e1f073b1131e5183b55af97e
user <user@example.com>
2026-07-14 15:52:26 +0800

    清理上游项目多余文件

--- app/config.py
+++
@@ -1,8 +0,0 @@
-version = '0.2.5'
-contact = '474379264'
-description = [
-    '1. 支持获取个人信息<br>',
-    '2. 支持显示聊天界面<br>',
-    '3. 支持导出聊天记录<br>&nbsp;&nbsp;&nbsp;&nbsp;* csv<br>&nbsp;&nbsp;&nbsp;&nbsp;* html<br>',
-    '4. 查找联系人<br>',
-]
